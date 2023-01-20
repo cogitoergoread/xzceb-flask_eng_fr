5bf372e7da51290126367b5c6f353dd9be12ce2a
Varga hupi József <31209755+cogitoergoread@users.noreply.github.com>
2023-01-20 05:35:30 +0100

    Server to translate E2f / F2E

--- final_project/server.py
+++ final_project/server.py
@@ -8,17 +8,22 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    translated_text=translator.englishToFrench(textToTranslate)
+    app.logger.debug(f"E2F To:{textToTranslate}, Transated:{translated_text}")
+    return translated_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    translated_text=translator.frenchToEnglish(textToTranslate)
+    app.logger.debug(f"F2E To:{textToTranslate}, Transated:{translated_text}")
+    return translated_text
 
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    pass
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
